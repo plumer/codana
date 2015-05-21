1dc123f9e2e1e423f98bbb388e43e1e5a4759b2c
liukun <liukunaa22@126.com>
2015-05-21 13:05:11 +0800

    12

--- java-package.py
+++ java-package.py
@@ -2,25 +2,28 @@ import understand
 
 projects=["tomcat6.0.0","tomcat6.0.43","tomcat7.0.0","tomcat7.0.61","tomcat8.0.0","tomcat8.0.21"]
 for project in projects:
-    db=understand.open(project+".udb")
+    db=understand.open("..//"+project+".udb")
     package={}
     edges=[]
-    file=open(project+"_pack.txt","w+");
-    for ent in db.ents("package"):
+    file=open(".//tomcat_history//"+project+"//tomcat_pack.txt","w");
+    p=sorted(db.ents("package"),key=lambda ent: ent.name())
+    for ent in p:
         l=ent.refs("contain");
-        file.write("%s  %d\n"%(ent.name(),len(l)))
+        m=ent.metric(['CountDeclClass','CountDeclFile','CountLineCode'])       
+        file.write("%s\t%d\t%s\t%s\t%s\n"%(ent.name(),len(l),m['CountDeclFile'],m['CountDeclClass'],m['CountLineCode']))
         for t in l:
-            package[t.file().name()]=ent
-            x=t.file().metric(['CountLine'])
-            file.write("\t%s\t%s\n"%(t.file().name(),x['CountLine']))
-    for ent in db.ents("java package"):
-        for t in ent.refs("contain"):
-            for x in t.file().depends().keys():
-                if x.name() in package:
-                    if[ent,package[x.name()]] not in edges:
-                        edges.append([ent,package[x.name()]])
-    file=open(project+"_pack_depends.txt","w+")
-    for x,y in edges:
-        file.write("%s   %s\n"%(x,y))
+                package[t.file().name()]=ent
+                x=t.file().metric(['CountLineCode','CountDeclClass','CountDeclMethod','CountDeclClassMethod'])
+                file.write("\t%s\t%s\t%s\t%s\t%s\n"%(t.file().name(),x['CountLineCode'],x['CountDeclClass'],x['CountDeclMethod'],x['CountDeclClassMethod']))
     file.close()
+    #for ent in p:
+    #   for t in ent.refs("contin"):
+    #        for x in t.file().depends().keys():
+    #            if x.name() in package:
+    #                if[ent,package[x.name()]] not in edges:
+    #                    edges.append([ent,package[x.name()]])
+    #file=open(project+"_pack_depends.txt","w+")
+    #for x,y in edges:
+    #    file.write("%s   %s\n"%(x,y))
+    #file.close()
     db.close()
--- package.py
+++ package.py
@@ -1,20 +1,42 @@
 #used for reading package info
 class packagereader:
-    def _init_(self,ver):
-        self.packages={}
-        self.files={}
-        file=open("tomcathistory//tomcat"+ver+"//tomcat_pack.txt","r")
+    def __init__(self,version):
+        self.__packages={}
+        self.__files={}
+        file=open(".//tomcat_history//tomcat"+version+"//tomcat_pack.txt","r")
         s=file.readline()
-        while not s==EOF:
-            x=s.split(' ')
-            self.packages[x[0]]=[]
+        while s!='':
+            x=s[:-1].split('\t')
+            self.__packages[x[0]]=x[2:]
             for i in range(int(x[1])):
                 t=file.readline()
-                a=t.split(' ')
-                self.packages[x[0]].append(a[0])
-                self.files[a[0]]=a[1:]
+                a=t[:-1].split('\t')
+                self.__packages[x[0]].append(a[1])
+                self.__files[a[1]]=a[2:]
             s=file.readline()
-    def getpackage(self,packagename):
-        return self.packages[packagename]
+        file.close()
+    def getpackagelist(self):
+        return self.__packages.keys()
+    def getpackagecontain(self,packagename):
+        return self.__packages[packagename][3:]
+    def getpackageinfo(self,packagename):
+        return self.__packages[packagename][0:3]
     def getfileinfo(self,filename):
-        return self.files[filename]
+        return self.__files[filename]
+if __name__=='__main__':
+    a=packagereader('6.0.43')
+    l=a.getpackagelist()
+    file=open("list.txt","w")
+    for i in l:
+
+        file.write(i)
+        for x in a.getpackageinfo(i):
+            file.write("\t%s"%(x))
+        file.write("\n")
+
+        for t in a.getpackagecontain(i):
+            file.write(t)
+            for x in a.getfileinfo(t):
+                file.write("\t%s"%(x))
+            file.write("\n")
+    file.close()
