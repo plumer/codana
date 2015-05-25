a332f6acc85f2dca0f17a92e6acdc925840cd1d8
Kai-Zhang <cs.zhangkai@outlook.com>
2015-05-25 21:22:23 +0800

    Add bug number interface in projectdata.py

--- projectdata.py
+++ projectdata.py
@@ -4,7 +4,8 @@ class DataManager:
     Attributes:
         packages (list of str): List of packages name
         files (list of str): List of all the files in the project
-        packagedict (dict): Map of packages(key) and filename(value)
+        packagedict (dict): Map of packages(key) and filenames(value)
+        filebugnum (dict): Map of filename(key) and bug numbers(value)
         fileattr (dict): Map of filename(key) and the attributes of the file(value)
         packageattr (dict): Map of package(key) and the attributes of the package(value)
     """
@@ -12,6 +13,7 @@ class DataManager:
         self.packagedict = {}
         self.fileattr = {}
         self.files = []
+        self.filebugnum = {}
         self.packageattr = {}
         datafile = open(r'tomcat_history/tomcat' + version + r'/tomcat_pack.txt', 'r')
         for packs in datafile:
@@ -30,6 +32,12 @@ class DataManager:
                 filenum = filenum + 1
                 if filenum >= int(packslice[1]):
                     break
+        datafile.close()
+        datafile = open(r'tomcat_history/tomcat' + version + r'/log.txt', 'r')
+        for record in datafile:
+            recordslice = record.strip(' \t\n').split('\t')
+            self.filebugnum[recordslice[0]] = int(recordslice[1])
+        datafile.close()
         self.packages = self.packagedict.keys()
 
     def packPackageAttr(self, attrs):
@@ -56,11 +64,23 @@ class DataManager:
     def getFilesOfPackage(self, package):
         return self.packagedict[package]
 
+    def getPackageOfFile(self, filename):
+        return self.filedict[filename]
+
     def getFileAttr(self, filename):
         return self.fileattr[filename]
 
     def getPackageAttr(self, package):
         return self.packageattr[package]
+
+    def getBugNumberOfFile(self, filename):
+        return self.filebugnum[filename]
+
+    def getBugNumberOfPackage(self, package):
+        bugnum = 0
+        for filename in self.packagedict[package]:
+            bugnum = bugnum + self.filebugnum[filename]
+        return bugnum
 
 if __name__ == '__main__':
     DataManager()
